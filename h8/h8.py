cda4eadd79e8c28271319eb3145f71b2d40e027c
sameer.reddy <sameer.reddy@servicenow.com>
2017-07-11 16:26:02 -0700

    completed all h's

--- h8/h8.py
+++ h8/h8.py
@@ -2,6 +2,7 @@ import pdb
 import pandas as pd
 import numpy as np
 from sklearn import svm
+from sklearn.model_selection import GridSearchCV
 
 import sys
 sys.path.append("..")
@@ -33,12 +34,12 @@ def create_labels(positive=None, negative='all', digits=None):
 	'''
 	Create the y labels for dataset x.
 	Parameters:
-	@param positive: define what digit should be +1
-	@param negative: define what digit should be -1. If doing one-versus-all don't define this parameter.
-	@param digits: array of digits. 
+		positive: define what digit should be +1
+		negative: define what digit should be -1. If doing one-versus-all don't define this parameter.
+		digits: array of digits. 
 
 	Returns: 
-	numpy array of +1, -1 and 0 (disregarded digits in one-vs-one). Label order corresponds to digits input order.
+	numpy array of +1, -1 and 0 (disregarded digits in one-vs-one).
 	'''
 	y = []
 	assert positive is not None and negative is not None and digits is not None
@@ -53,12 +54,17 @@ def create_labels(positive=None, negative='all', digits=None):
 
 get_x = lambda df: np.array(zip(df.intensity, df.symmetry))
 
-def error_wrt_label(label, df, m):
-	assert label is not None
-	df = df[df.digits == label]
-	predictions = m.predict(get_x(df))
-	print predictions
-	return len(np.where(x != label)) / float(len(predictions))
+def one_v_all_label(d, df):
+	return np.array([1 if num == d else -1 for num in df.digits])
+
+def one_v_one_label(pos, neg, df):
+	y = []
+	for num in df.digits:
+		if num == pos:
+			y.append(1)
+		elif num == neg:
+			y.append(-1)
+	return np.array(y)
 
 if __name__ == '__main__':
 	train = get_train()
@@ -66,16 +72,34 @@ if __name__ == '__main__':
 	dig = np.arange(0, 10)
 	models = []
 	boldprint("One-vs-all classifiers")
-	y = np.array(train.digits)
-	x = get_x(train)
-	model = svm.SVC(C=0.01, kernel="poly", degree=2, decision_function_shape='ovr') 
+	print "Digit", "\tIn-Sample Err", "\tOut-Sample Err"
+	for d in dig:
+		y = one_v_all_label(d, train)
+		x = get_x(train)
+		model = svm.SVC(C=0.01, kernel="poly", degree=2, decision_function_shape='ovr', gamma=1, coef0=1) 
+		model.fit(x, y)
+		models.append(model)
+		print d, "\t", round(1 - model.score(x, y), 4), "\t\t", round(1 - model.score(get_x(test), one_v_all_label(d, test)), 4)
+
+	boldprint("Making 1 v 5 classifiers...")
+	train_1_5 = train[train.digits.isin([1, 5])]
+	test_1_5 = test[test.digits.isin([1, 5])]
+	y = one_v_one_label(1, 5, train_1_5)
+	x = get_x(train_1_5)
+	for C in [0.001, 0.01, 0.1, 1]:
+		model = svm.SVC(C=C, kernel="poly", degree=2, decision_function_shape='ovr', gamma=1, coef0=1) 
+		model.fit(x, y)
+		print "C =", C
+		print "Ein =", 1 - model.score(x, y)
+		print "Eout =", 1 - model.score(get_x(test_1_5), one_v_one_label(1, 5, test_1_5))
+		print "Num Support Vectors =", model.n_support_
+		print ""
+
+	boldprint("Cross validation...")
+	model = GridSearchCV(svm.SVC(kernel='poly', degree=2, gamma=1, coef0=1, decision_function_shape='ovr'), [{'C' : [0.0001, 0.001, 0.01, 0.1, 1]}], cv=10)
 	model.fit(x, y)
-	# for d in dig:
-	# 	print d, error_wrt_label(d, train, model)
-	print error_wrt_label(1, train, model)
-	print model.n_support_
-	# out_sample_err = calc_error_rate(create_labels(positive=d, digits=test.digit), model.predict(get_x(test)))
-	# in_sample_err = calc_error_rate(y, model.predict(x))
-	# print "Digit | In Sample Err | Out Sample Err" 
-	# print 0, '\t', round(in_sample_err, 4), '\t\t', round(out_sample_err, 4)
+	print model.best_params_
+	print 1 - model.score(x, y)
+	print 1 - model.score(get_x(test_1_5), one_v_one_label(1, 5, test_1_5))
+
 
